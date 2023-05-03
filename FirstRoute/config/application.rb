1562d3d619df28dbbbdd5eea9a47bbae692d79a6
Thomas Ly <Thomaslyy13@gmail.com>
2023-05-03 14:28:22 -0700

    finished routes

--- FirstRoute/config/application.rb
+++ FirstRoute/config/application.rb
@@ -22,6 +22,7 @@ module FirstRoute
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.action_controller.default_protect_from_forgery = false
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -32,6 +33,7 @@ module FirstRoute
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
+
     config.generators.system_tests = nil
   end
 end
--- FirstRoute/config/routes.rb
+++ FirstRoute/config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get '/users', to: 'users#index', as: 'users'
+  post '/users', to: 'users#create'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+
+  # resources :users
 end
