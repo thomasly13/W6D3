888665a336c824294843ccc6751edf594178fb66
a-wong-8 <awong88@berkeley.edu>
2023-05-03 17:33:46 -0700

    ..

--- FirstRoute/app/controllers/users_controller.rb
+++ FirstRoute/app/controllers/users_controller.rb
@@ -33,7 +33,8 @@ class UsersController < ApplicationController
         redirect_to users_url
     end
     
+    private 
     def user_params
-        params.require(:user).permit(:name, :email)
+        params.require(:user).permit(:username)
     end
 end
